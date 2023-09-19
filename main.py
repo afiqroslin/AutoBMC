0241a98de796ebcc9b26a6022171fa0b26333811
Afiq <mafiqbinrosl@gmail.com>
2023-09-19 14:44:34 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,55 @@
+# Project: Automate BMC Systems Login
+# Author: Afiq Roslin
+# GitHub: Afiq Roslin
+
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+import time
+
+path = r"C:\Users\afiq0\PycharmProjects\AutoBMC\chromedriver_win32\chromedriver.exe"  # path to chromedriver program
+options = webdriver.ChromeOptions()  # Chrome WebDriver API
+options.add_experimental_option("detach", True)  # detach chrome from program after open
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+caps = options.to_capabilities()
+caps["acceptInsecureCerts"] = True  # proceed to unsafe
+
+
+def AutoBMC(BMC_IP, USERNAME, USER, PWD):
+    print(f"bmc: {BMC_IP}")
+    print(f"username {USERNAME}")
+
+    print("Redirecting...")
+    driver = webdriver.Chrome(options=options)
+    driver.get("http://" + BMC_IP)
+
+    wait = WebDriverWait(driver, 20)
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@autocomplete='username']"))).send_keys(USERNAME)
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@role='button'][contains(.,'Next')]"))).click()
+
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@autocomplete='on']"))).send_keys(USER)
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@role='button'][contains(.,'Next')]"))).click()
+
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@autocomplete='current-password']"))).send_keys(PWD)
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@role='button'][contains(.,'Log in')]"))).click()
+
+
+if __name__ == '__main__':
+    while True:
+        df = pd.read_excel(r'C:\Users\afiq0\PycharmProjects\AutoBMC\autobmc.xlsx')  # read excel file contains hostname
+        print(df.iloc[1:, 0])
+        print('\nChoose Hostname\n      or\nPress enter to exit..')
+        userInput = int(input())
+
+        if not userInput:
+            continue
+
+        if userInput in df.index:
+            AutoBMC(BMC_IP=df.iloc[userInput].BMC_IP, USERNAME=df.iloc[userInput].Username,
+                    USER=df.iloc[userInput].user, PWD=df.iloc[userInput].Password)
+
+        else:
+            continue

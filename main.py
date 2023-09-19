c78f94ac08818672e8f5799f07f352ad64888f2f
Afiq <mafiqbinrosl@gmail.com>
2023-09-19 14:49:10 +0800

    add comment

--- main.py
+++ main.py
@@ -24,15 +24,18 @@ def AutoBMC(BMC_IP, USERNAME, USER, PWD):
 
     print("Redirecting...")
     driver = webdriver.Chrome(options=options)
-    driver.get("http://" + BMC_IP)
+    driver.get("http://" + BMC_IP) #Get server IP address/domain name
 
+    # Insert username and click next
     wait = WebDriverWait(driver, 20)
     wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@autocomplete='username']"))).send_keys(USERNAME)
     wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@role='button'][contains(.,'Next')]"))).click()
 
+    # Twitter will detect automation as suspicious activity, so need to bypass by providing the info needed
     wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@autocomplete='on']"))).send_keys(USER)
     wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@role='button'][contains(.,'Next')]"))).click()
 
+    # Insert password fill and click login
     wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@autocomplete='current-password']"))).send_keys(PWD)
     wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@role='button'][contains(.,'Log in')]"))).click()
 
@@ -47,6 +50,7 @@ if __name__ == '__main__':
         if not userInput:
             continue
 
+        # Take input (ip address, username, password) from the excel file
         if userInput in df.index:
             AutoBMC(BMC_IP=df.iloc[userInput].BMC_IP, USERNAME=df.iloc[userInput].Username,
                     USER=df.iloc[userInput].user, PWD=df.iloc[userInput].Password)
